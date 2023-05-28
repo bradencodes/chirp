4cf972c76f3bfa80bfab014c2e65d2473f7d5885
Braden Walker <bradencodes@gmail.com>
2023-05-27 20:25:03 -0700

    boost size of text

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -53,7 +53,7 @@ const PostView = (props: PostWithUser) => {
           <span>·</span>
           <span className="font-thin">{dayjs(post.createdAt).fromNow()}</span>
         </div>
-        <span>{post.content}</span>
+        <span className="text-2xl">{post.content}</span>
       </div>
     </div>
   );
