f0109c47edac769af9afe63f2b3ddf3c5f2e35d3
Braden Walker <bradencodes@gmail.com>
2023-05-29 13:19:11 -0700

    get profile from clerk

--- src/server/api/routers/posts.ts
+++ src/server/api/routers/posts.ts
@@ -1,4 +1,3 @@
-import { type User } from "@clerk/nextjs/dist/api";
 import { clerkClient } from "@clerk/nextjs/server";
 import { TRPCError } from "@trpc/server";
 import { z } from "zod";
@@ -8,12 +7,7 @@ import {
   privateProcedure,
   publicProcedure,
 } from "~/server/api/trpc";
-
-const filterUserForClient = (user: User) => {
-  const { id, username, profileImageUrl } = user;
-
-  return { id, username, profileImageUrl };
-};
+import { filterUserForClient } from "~/server/helpers/filterUserForClient";
 
 import { Ratelimit } from "@upstash/ratelimit"; // for deno: see above
 import { Redis } from "@upstash/redis";
